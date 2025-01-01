d2ec64890332ae1789f87498bed2b9e454ef2ab2
swhipz <97738622+swhipz@users.noreply.github.com>
2025-01-01 22:10:13 +0100

    published

--- 3-whitespace/whitespace/src/App.jsx
+++ 3-whitespace/whitespace/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Gallery from './components/gallery/Gallery'
-import Info from './components/info/Info'
+import Gallery from './components/gallery/Gallery';
+import Info from './components/info/Info';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Gallery />
       <Info />
       <Footer />
--- 3-whitespace/whitespace/src/components/info/Info.jsx
+++ 3-whitespace/whitespace/src/components/info/Info.jsx
@@ -5,8 +5,8 @@ const Info = () => {
         <>
         <div className="wrapper">
         <div className="infobox">
-            <h3 class="headline">Thank you</h3>
-            <p class="infoText">I am thrilled to display some of my work in this whitespace-format. I find it quite elegant, and it was a fun sunday project.
+            <h3 className="headline">Thank you</h3>
+            <p className="infoText">I am thrilled to display some of my work in this whitespace-format. I find it quite elegant, and it was a fun sunday project.
                 If you do <i>do</i> want my photography services, please feel free to contact me.</p>
         </div>
         </div>
